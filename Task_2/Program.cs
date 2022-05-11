a96b3f3389ab8872d51fd868cecc3ba0a0f2a8d3
Eugeny <76texac@gmail.com>
2022-05-11 21:58:13 +0300

    исправил int вместо var

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -2,7 +2,7 @@
 // Найдите сумму элементов, стоящих на нечётных позициях.
 
 Console.Clear();
-var array = new int[5];
+int[] array = new int[5];
 int sum = 0;
 for (int i = 0; i < array.Length; i++)
 {
